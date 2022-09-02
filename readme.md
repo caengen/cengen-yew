0f8448b5193cafd7d183eb1c6bec93bd5f8c61e1
Christian Engen <chris.a.engen@gmail.com>
2022-09-02 20:52:24 +0200

    init

---
+++ readme.md
@@ -0,0 +1,13 @@
+# Yew app
+
+## Dependencies
+
+install these globally `cargo`, `trunk`, `tailwindcss`
+
+## Run app
+
+Serve app
+`trunk serve`
+
+Tailwind watcher
+`tailwindcss -w -o ./styles/output.css`
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{html,rs}", "./styles/**/*.css"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
