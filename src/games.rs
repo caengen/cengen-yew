6a5add40a3f3c1804b7ca8463175a0a3bd614466
Christian Engen <chris.a.engen@gmail.com>
2022-11-24 21:51:08 +0100

    add games page

---
+++ src/games.rs
@@ -0,0 +1,27 @@
+use super::Page;
+use yew::prelude::*;
+
+#[function_component(GamesPage)]
+pub fn games_page() -> Html {
+    html! {
+      <Page>
+        <div>
+          <p class={"pb-12"}>{"A compilation of some games I've made while learning game dev concepts and frameworks."}</p>
+          <section class={"pb-4 border-b-2 border-b-gray-400"}>
+          <div class={"flex column justify-between"}>
+            <h1 class={"text-xl pb-3"}>{"Yet another Tetris clone"}</h1>
+            <small class={"text-gray-400"}>{"October 2022"}</small>
+            </div>
+            <div class={"flex row justify-between"}>
+              <div class={"pr-5"}>
+                <h2 class={"text-base"}>{"Written in Rust, made with Macroquad"}</h2>
+                <p class={"text-base"}>{"
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed massa orci, condimentum quis orci et, tempor tincidunt diam. Nullam dictum, lacus et pulvinar euismod, tortor neque commodo elit."}</p>
+              </div>
+              <img class={"w-5/12 rounded-xl"} src="https://github.com/caengen/tetris-rs/blob/master/demo/demo.gif?raw=true" alt="showcase tetris"/>
+            </div>
+          </section>
+        </div>
+      </Page>
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,19 @@ use yew_router::prelude::*;
 mod routes;
 use page::Page;
 mod page;
+use games::*;
 use home::*;
 mod compressed_posts;
 mod dark;
 use dark::*;
+mod games;
 mod hero;
 mod home;
 
 fn switch(routes: &Route) -> Html {
     match routes {
         Route::Home => html! { <HomePage /> },
-        Route::Games => html! { <Page><h1>{"Games"}</h1> </Page>},
+        Route::Games => html! { <GamesPage />},
         Route::Work => html! { <Page><h1>{"Work"}</h1> </Page>},
         Route::About => html! { <Page><h1>{"About"}</h1> </Page>},
         Route::NotFound => html! { <Page><h1>{"NotFound"}</h1> </Page>},
@@ -29,6 +31,7 @@ fn app() -> Html {
             <BrowserRouter>
                 <Switch<Route> render={Switch::render(switch)} />
             </BrowserRouter>
+            <script src="https://not-fl3.github.io/miniquad-samples/mq_js_bundle.js"></script>
         </DarkMode>
     }
 }
